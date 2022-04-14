301201fd79454cda2f84898d202ae58663858ebe
N_G <1460412+guuuuo@users.noreply.github.com>
2022-04-14 14:44:31 +0800

    feat: Add GET /v1/posts/{postId}

--- application/src/main/java/com/amoylabs/blog/post/controller/PostsController.java
+++ application/src/main/java/com/amoylabs/blog/post/controller/PostsController.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/posts")
@@ -17,6 +20,11 @@ public class PostsController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity loadPosts() {
         return ResponseEntity.ok().body(postsService.loadPosts());
+    }
+
+    @GetMapping(value = "/{postId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity loadPost(@PathVariable UUID postId) {
+        return ResponseEntity.ok().body(postsService.loadPost(postId));
     }
 
     @Autowired
--- application/src/main/java/com/amoylabs/blog/post/service/PostsService.java
+++ application/src/main/java/com/amoylabs/blog/post/service/PostsService.java
@@ -11,17 +11,21 @@ import java.util.UUID;
 @Service
 public class PostsService {
 
+    public Post loadPost(UUID postId) {
+        return mockPost(postId, "Title x", "Content x");
+    }
+
     public List<Post> loadPosts() {
         return Arrays.asList(
-                mockPost("Title 1", "Content 1"),
-                mockPost("Title 2", "Content 2"),
-                mockPost("Title 3", "Content 3")
+                mockPost(UUID.randomUUID(), "Title 1", "Content 1"),
+                mockPost(UUID.randomUUID(), "Title 2", "Content 2"),
+                mockPost(UUID.randomUUID(), "Title 3", "Content 3")
         );
     }
 
-    private Post mockPost(String title, String content) {
+    private Post mockPost(UUID postId, String title, String content) {
         Post post = new Post();
-        post.setPostId(UUID.randomUUID());
+        post.setPostId(postId);
         post.setUserId(UUID.fromString("c7b2eb87-f5c5-4558-a568-adba5f647690"));
         post.setTitle(title);
         post.setContent(content);
